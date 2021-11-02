d401879f445070040457ceb8e8c4e1be37ab98e0
Cheng Ye <2540253y@student.gla.ac.uk>
2021-11-02 02:42:56 +0100

    json file serialized implementation

--- src/function/connect.rs
+++ src/function/connect.rs
@@ -1,6 +1,18 @@
 use ssh2::Session;
 use std::net::{TcpStream, ToSocketAddrs};
+use structopt::StructOpt;
+extern crate serde;
+use std::fs::File;
+use std::io::Read;
+use serde::{Deserialize, Serialize};
 
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Groups{
+    pub groups: Vec<Vec<Connection>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Connection
 {
     pub hostname : String,
@@ -15,13 +27,12 @@ pub trait ConnectSsh
 
 impl ConnectSsh for Connection {
     fn connect_ssh(&self) ->Session {
-        //let host_ip: Vec<_> = self.hostname.to_socket_addrs().unwrap().collect();
         let tcp = TcpStream::connect(&self.hostname).unwrap();
         let mut sess = Session::new().unwrap();
         sess.set_tcp_stream(tcp);
         sess.handshake().unwrap();
 
-        sess.userauth_password(&*self.username, &*self.password).unwrap();
+        sess.userauth_password(&self.username, &self.password).unwrap();
         assert!(sess.authenticated());
         sess
     }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,28 @@
 mod function;
 
 use ssh2::Session;
+use std::fs::File;
+use std::io::copy;
+use std::io::stdout;
 use std::io::prelude::*;
+use structopt::StructOpt;
 
+use crate::function::connect::Groups;
 use crate::function::connect::Connection;
 use crate::function::connect::ConnectSsh;
 use crate::function::command_used::Command;
 use crate::function::command_used::ExCommand;
 
+
 fn main()  {
-    let conn = Connection{hostname : String::from("hostname:22"),
-        username : String::from("username"),
-        password : String::from("password")};
-    let exec = Command{sess: Session::from(conn.connect_ssh()),
-        comm: String::from("command")};
-    let s = exec.ex_command();
+    let mut file = File::open("config.json").unwrap();
+    let mut data = String::new();
+    file.read_to_string(&mut data).unwrap();
+
+    let g: Groups = serde_json::from_str(&data).unwrap();
+    let conn = &g.groups[0][0];
+
+    let exec: Command = serde_json::from_str(&data).unwrap();
+    let s = exec.ex_command(conn.connect_ssh());
     print!("{}",s);
-
-
 }
